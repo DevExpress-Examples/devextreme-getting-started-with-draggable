473db1fd46c441b9f326a3d1d283348e66d47abe
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-12-26 15:20:24 +0400

    Add jQuery and Remove ASP

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,61 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
-    },
-  });
+    const notes = ["#note-1", "#note-2", "#note-3", "#note-4"];
+    let z = 1;
+
+  $("#note-1").dxDraggable({
+    onDragStart: handleDragStart,
+    onDragMove: handleDragMove,
+    onDragEnd: handleDragEnd,
+    boundary: ".board",
+    group: "1",
+  }).on("click", handleClick);
+
+  $("#note-2").dxDraggable({
+    onDragStart: handleDragStart,
+    onDragMove: handleDragMove,
+    onDragEnd: handleDragEnd,
+    group: "1",
+    boundary: ".board",
+  }).on("click", handleClick);
+
+  $("#note-3").dxDraggable({
+    onDragStart: handleDragStart,
+    onDragMove: handleDragMove,
+    onDragEnd: handleDragEnd,
+    group: "1",
+    boundary: ".board",
+  }).on("click", handleClick);
+
+  $("#note-4").dxDraggable({
+    onDragStart: handleDragStart,
+    onDragMove: handleDragMove,
+    onDragEnd: handleDragEnd,
+    boundary: ".board",
+    group: "1",
+  }).on("click", handleClick);
+
+  function changeZIndex(el) {
+    z++;
+    el.css("z-index", z);
+  }
+
+  function handleClick(e) {
+    changeZIndex($(e.currentTarget));
+  }
+
+  function handleDragStart(e) {
+    changeZIndex($(e.element[0]));
+  }
+
+  function handleDragMove(e) {
+    if (e.toComponent !== e.component) {
+      e.toComponent.element().css("outline", "1px dashed red");
+    } else {
+      notes.forEach((el) => $(el).css('outline', ''));
+    }
+  }
+
+  function handleDragEnd() {
+    notes.forEach((el) => $(el).css('outline', ''));
+  }
 });
