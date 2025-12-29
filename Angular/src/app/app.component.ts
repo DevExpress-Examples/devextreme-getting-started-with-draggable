bb67b26cf051ee1b152468e0b429f50906ed572c
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-12-29 14:21:32 +0400

    Add Angular

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import { DxDraggableTypes } from 'devextreme-angular/ui/draggable';
+import { on, trigger } from "devextreme/events";
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,41 @@ import { ClickEvent } from 'devextreme/ui/button';
 export class AppComponent {
   title = 'Angular';
 
-  counter = 0;
+  notes = ["#note-1", "#note-2", "#note-3", "#note-4"];
 
-  buttonText = 'Click count: 0';
+  z = 1;
 
-  onClick(_e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  changeZIndex(el: HTMLElement) {
+    this.z++;
+    el.style.zIndex = this.z.toString();
+  }
+
+  handleClick(e: any) {
+    console.log(e);
+    //this.changeZIndex(e.currentTarget.element());
+  }
+
+  handleInit(e: any) {
+    on(e.element, 'dxclick', this.handleClick);
+
+    on(document.body, 'dxclick', this.handleClick);
+    document.body.addEventListener('click', this.handleClick);
+  }
+
+
+  handleDragStart(e: DxDraggableTypes.DragStartEvent) {
+    this.changeZIndex(e.element);
+ }
+
+  handleDragMove(e: DxDraggableTypes.DragMoveEvent) {
+    if (e.toComponent !== e.component) {
+      e.toComponent.element().style.outline = '1px dashed red';
+    } else {
+      this.notes.forEach((el) => { document.querySelector<HTMLElement>(el)?.style.setProperty('outline', ''); });
+    }
+  }
+
+  handleDragEnd(e: DxDraggableTypes.DragEndEvent) {
+    this.notes.forEach((el) => { document.querySelector<HTMLElement>(el)?.style.setProperty('outline', ''); });
   }
 }
