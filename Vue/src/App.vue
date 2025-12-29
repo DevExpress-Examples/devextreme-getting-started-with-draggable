02052a715b6b93dc51a84cb64d4c934792e417db
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-12-29 16:48:16 +0400

    Add Vue

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div class="main">
+  <div class="demo-container">
     <RouterView/>
   </div>
 </template>
--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,28 +1,110 @@
-<script setup lang="ts">
-import { computed, ref } from 'vue';
-
-import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import DxButton from 'devextreme-vue/button';
-
-const props = defineProps({
-  text: {
-    type: String,
-    default: 'count',
-  },
-});
-const count = ref(0);
-const buttonText = computed < string > (
-  () => `Click ${props.text}: ${count.value}`
-);
-function clickHandler() {
-  count.value += 1;
-}
-</script>
 <template>
-  <div>
-    <DxButton
-      :text="buttonText"
-      @click="clickHandler"
-    />
-  </div>
+  <div class="board dx-theme-fluent-typography">
+
+        <DxDraggable
+          id="note-1"
+          group="cards"
+          class="card"
+          boundary=".board"
+          @initialized="handleInit"
+          @drag-start="handleDragStart"
+        >
+          <div class="color-indicator blue"></div>
+          <div class="text-container">
+            <div class="body-text-box">Install New Router in Dev Room</div>
+            <div class="detail-text-box">Amelia Harper</div>
+          </div>
+        </DxDraggable>
+
+        <DxDraggable
+          id="note-2"
+          group="cards"
+          class="card"
+          boundary=".board"
+          @initialized="handleInit"
+          @drag-start="handleDragStart"
+        >
+          <div class="color-indicator green"></div>
+          <div class="text-container">
+            <div class="body-text-box">Launch New Website</div>
+            <div class="detail-text-box">Brett Wade</div>
+          </div>
+        </DxDraggable>
+
+        <DxDraggable
+          id="note-3"
+          group="cards"
+          class="card"
+          boundary=".board"
+          @initialized="handleInit"
+          @drag-start="handleDragStart"
+        >
+          <div class="color-indicator red"></div>
+          <div class="text-container">
+            <div class="body-text-box">Prepare 2026 Marketing Plan</div>
+            <div class="detail-text-box">Robert Reagan</div>
+          </div>
+        </DxDraggable>
+
+        <DxDraggable
+          id="note-4"
+          group="cards"
+          class="card"
+          boundary=".board"
+          @initialized="handleInit"
+          @drag-start="handleDragStart"
+        >
+          <div class="color-indicator yellow"></div>
+          <div class="text-container">
+            <div class="body-text-box">Approve Personal Computer Upgrade Plan</div>
+            <div class="detail-text-box">Bart Arnaz</div>
+          </div>
+        </DxDraggable>
+
+    </div>
 </template>
+
+<script setup lang="ts">
+import 'devextreme/dist/css/dx.fluent.blue.light.css';
+import { DxDraggable, type DxDraggableTypes } from 'devextreme-vue/draggable';
+import { on } from 'devextreme/events';
+import { type EventObject } from 'devextreme/common/core/events';
+
+let z = 1;
+
+function changeZIndex(element: HTMLElement) {
+  z++;
+  element.style.zIndex = z.toString();
+}
+
+function handleClick(e: EventObject) {
+  changeZIndex(e.currentTarget as HTMLElement);
+}
+
+function handleDragEnter(e: EventObject) {
+  const target: HTMLElement = e.target as HTMLElement;
+
+  target.style.outline = "1px dashed red";
+}
+
+function handleDragStop(e: EventObject) {
+  const target: HTMLElement = e.target as HTMLElement;
+
+  target.style.outline = "";
+}
+
+function handleInit(e: DxDraggableTypes.InitializedEvent) {
+  on(e.element!, 'click', handleClick);
+
+  on(e.element!, 'dxdragenter', handleDragEnter);
+
+  on(e.element!, 'dxdragleave', handleDragStop);
+  on(e.element!, 'dxdrop', handleDragStop);
+}
+
+function handleDragStart(e: DxDraggableTypes.DragStartEvent) {
+  changeZIndex(e.element);
+}
+
+</script>
+
